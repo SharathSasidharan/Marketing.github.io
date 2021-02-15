51b95b06bdeac9826d4e50388b060e6ac7cb43c2
STEVEN <sasidharansteven@gmail.com>
2021-02-15 11:40:07 +0530

    added Marketing file

---
+++ index.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+  
+    
+    if(this.scrollY > 500){
+        $('.scroll-up').addClass("show")
+    }else{
+        $('.scroll-up').removeClass("show")
+    }
+    
+    
+    });
+    
+    
+    
+    
+    //SLIDE UP SCRIPT
+    $('.scroll-up').click(function(){
+     $('html').animate({scrollTop: 0})
+    });
+    
+
+
+    //MENU BUTTON SCRIPT
+    
+    $('.menu-btn').click(function(){
+    $('.navbar .menu').toggleClass("active")
+    $('.menu-btn i').toggleClass("active")
+    })
+    }); 
